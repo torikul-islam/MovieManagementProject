6147da4e60f469f2cc8f455aca875c6f496f7827
Pc <Pc@DESKTOP-765R19V>
2018-08-08 22:53:41 +0600

    Authentical and authorization of the application

--- MovieHunt/App_Start/FilterConfig.cs
+++ MovieHunt/App_Start/FilterConfig.cs
@@ -8,6 +8,8 @@ namespace MovieHunt
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
+
         }
     }
 }
--- MovieHunt/Controllers/HomeController.cs
+++ MovieHunt/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace MovieHunt.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         public ActionResult Index()
